8319100b53931aaae3bbfa56d1239a9a4a88a7d7
David Siemionko <dsiemionko4406@stu.d214.org>
2020-10-20 16:11:07 -0400

    Initial Commit

---
+++ GitHubTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubTesting
+//
+//  Created by user180860 on 10/20/20.
+//  Copyright © 2020 user180860. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
